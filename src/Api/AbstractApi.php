0c0e9e79c18a828efae2891b8a00d95592e2ea8d
Gildonei Mendes Anacleto Junior <gildonei.mendes@gmail.com>
2019-02-27 18:09:11 -0300

    Atualização de Endpoint
    Atualizado a versão dos endpoints homologação e produção para a v3

--- src/Api/AbstractApi.php
+++ src/Api/AbstractApi.php
@@ -16,14 +16,14 @@ abstract class AbstractApi
      *
      * @var string
      */
-    const ENDPOINT_PRODUCAO = 'https://www.asaas.com/api/v2';
+    const ENDPOINT_PRODUCAO = 'https://www.asaas.com/api/v3';
 
     /**
      * Endpoint Homologação
      *
      * @var string
      */
-    const ENDPOINT_HOMOLOGACAO = 'http://homolog.asaas.com/api/v2';
+    const ENDPOINT_HOMOLOGACAO = 'http://homolog.asaas.com/api/v3';
 
     /**
      * Http Adapter Instance
